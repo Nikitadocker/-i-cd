2fb312f40354da9408f7be99565264351e6c5981
nikita <nbarabanschikov@mts.ru>
2023-02-17 19:45:00 +0300

    add comments

--- view ci/bamboo-cd.sh
+++ view ci/bamboo-cd.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-for i in $(cat deploy.list)
+for i in $(cat deploy.list) # перебираем циклом for cписок наших deployment
   do
   echo "start get status"
-    if $(helm status $i -n $1 > /dev/null 2>&1)
+    if $(helm status $i -n $1 > /dev/null 2>&1) #  выводим статус релиза в нашем namespace
       then
       echo "STATUS: OK!"
-        if $(grep -rn "b_$i" $i/$5/values.yaml | grep "$4" > /dev/null 2>&1)
+        if $(grep -rn "b_$i" $i/$5/values.yaml | grep "$4" > /dev/null 2>&1) # ищем с помощью grep имя ветки
         echo "find branch in values"
           then
-          rel=$(helm list -n $1 | grep $i | awk '{print $10}')
+          rel=$(helm list -n $1 | grep $i | awk '{print $10}') # присваиваем значение APP VERSION из helm list 
           echo "rel: $rel"
-          conf=$(grep appVersion $i/$5/Chart.yaml | cut -d' ' -f 2 )
+          conf=$(grep appVersion $i/$5/Chart.yaml | cut -d' ' -f 2 ) # ищем с помощью grep значение APP VERSION в Сhart и присваем это значение
           echo "conf: $conf"
-          if [ $conf != $rel ]
+          if [ $conf != $rel ] # если значение  APP VERSION не равны 
           echo "HAS DIFF!!!"
             then helm lint $i/$5/
-            helm upgrade --wait --atomic --timeout 4m --set domain=$2 $i $i/$5/ -n $1
+            helm upgrade --wait --atomic --timeout 4m --set domain=$2 $i $i/$5/ -n $1 # то выполнить ugrade
             helm history $i -n $1 --max 3
             sleep 5
           elif [ $conf == $rel ]
